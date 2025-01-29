2d085bb88df03e41bd96a9d39537040351d4b02f
AlbertPlaPlanas <38904278+AlbertPlaPlanas@users.noreply.github.com>
2025-01-29 09:53:04 +0100

    Update setup_py_env.sh to py 3.10

--- setup_py_env.sh
+++ setup_py_env.sh
@@ -8,7 +8,7 @@ PY_ENV_PATH=$1
 # Create Python environment
 if ! conda info --envs | grep -q "$PY_ENV_PATH"; then
     echo "Environment $PY_ENV_PATH does not exist. Creating..."
-    conda create --prefix $PY_ENV_PATH python=3.9
+    conda create --prefix $PY_ENV_PATH python=3.10
     conda activate $PY_ENV_PATH
     pip install -r requirements.txt
 fi
